2e2415d1122bf09e9c35c3e6ffc944471a15b3f2
Javier Francisco Cuadra <32556567+javiercuadra@users.noreply.github.com>
2023-11-01 20:50:25 -0500

    set recipe for virtual_chef

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from data_structures.VirtualChef import VirtualChef
+from utils.create_recipe import create_recipe
 from utils.fetch_recipe import fetch_recipe
 from utils.html_parser import parse_html
 
 if __name__ == "__main__":
+    virtual_chef = VirtualChef()
+
     # url = 'https://www.epicurious.com/recipes/food/views/ba-syn-creamed-spinach-stuffed-meatloaf'
     # url = 'https://www.epicurious.com/recipes/food/views/my-favorite-simple-roast-chicken-231348'
     url = 'https://www.epicurious.com/recipes/food/views/mashed-potatoes-recipe'
@@ -10,15 +14,10 @@ if __name__ == "__main__":
 
     parsed_html = parse_html(webpage_content)
 
-    recipe_ingredients = parsed_html["recipeIngredient"]
-    recipe_instructions = parsed_html["recipeInstructions"]
+    for key in parsed_html:
+        print(key)
+        print()
 
-    print()
-    for ingredient in recipe_ingredients:
-        print(ingredient)
+    recipe = create_recipe(parsed_html)
 
-    print()
-
-    for instruction in recipe_instructions:
-        print(instruction["text"])
-        print("--")
+    virtual_chef.set_recipe(recipe)
