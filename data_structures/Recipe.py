f7ffe89323158c19236a5e4094e2adc17e4c762c
Javier Francisco Cuadra <32556567+javiercuadra@users.noreply.github.com>
2023-11-07 21:06:31 -0600

    reorder recipe

--- data_structures/Recipe.py
+++ data_structures/Recipe.py
@@ -1,5 +1,6 @@
-from typing import Dict, Optional, Union
+from typing import Dict, Union
 from data_structures.DoublyLinkedList import DoublyLinkedList
+from data_structures.Ingredient import Ingredient
 
 class Recipe:
     def __init__(self) -> None:
@@ -10,43 +11,33 @@ class Recipe:
         self.__total_time = {}
         self.__instruction_count = 0
 
-    def get_name(self) -> str:
-        return self.__name
-
-    def get_ingredients(self) -> Dict["str", Dict["str", Union["float", "str"]]]:
-        return self.__ingredients
-
-    def get_instructions(self) -> DoublyLinkedList:
-        return self.__instructions
-
-    def get_instruction_count(self) -> int:
-        return self.__instruction_count
-
     def set_name(self, name: str) -> None:
         self.__name = name
 
-    def add_ingredient(self, name: str, quantity: float, metric: Optional["str"]=None) -> None:
-        ingredient_data = {
-            "quantity": quantity,
-            "metric": metric
-        }
-        self.__ingredients[name] = ingredient_data
+    def get_name(self) -> str:
+        return self.__name
 
-    def get_ingredient_info(self, ingredient: str) -> Dict["str", Union["float", "str"]]:
-        return self.get_ingredients()[ingredient]
+    def add_ingredient(self, name: str, ingredient: Ingredient) -> None:
+        self.__ingredients[name] = ingredient
 
-    def get_ingredient_quantity(self, ingredient: str) -> float:
-        return self.get_ingredient_info(ingredient)["quantity"]
+    def get_ingredients(self) -> Dict["str", Ingredient]:
+        return self.__ingredients
 
-    def get_ingredient_metric(self, ingredient: str) -> str:
-        return self.get_ingredient_info(ingredient)["metric"]
+    def get_ingredient(self, ingredient_name: str):
+        return self.get_ingredients()[ingredient_name]
 
     def add_instruction(self, instruction: str) -> None:
         self.__instructions.append(instruction)
         self.__instruction_count = self.get_instruction_count() + 1
 
+    def get_instructions(self) -> DoublyLinkedList:
+        return self.__instructions
+
     def get_instruction(self, index: int) -> str:
         self.__instructions.get_data_at(index)
+
+    def get_instruction_count(self) -> int:
+        return self.__instruction_count
 
     def add_total_time(self, quantity: float, metric: str) -> None:
         time_data = {
@@ -54,6 +45,15 @@ class Recipe:
             "metric": metric
         }
         self.__total_time = time_data
+
+    def get_total_time(self) -> Dict["str", Union["float", "str"]]:
+            return self.__total_time
+
+    def get_total_time_quantity(self) -> float:
+        return self.get_total_time()["quantity"]
+
+    def get_total_time_metric(self) -> str:
+        return self.get_total_time()["metric"]
 
     def add_cook_time(self, quantity: float, metric: str) -> None:
         time_data = {
@@ -70,12 +70,3 @@ class Recipe:
 
     def get_cook_time_metric(self) -> str:
         return self.get_cook_time()["metric"]
-
-    def get_total_time(self) -> Dict["str", Union["float", "str"]]:
-        return self.__total_time
-
-    def get_total_time_quantity(self) -> float:
-        return self.get_total_time()["quantity"]
-
-    def get_total_time_metric(self) -> str:
-        return self.get_total_time()["metric"]
