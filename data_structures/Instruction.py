44768105d197e226627ee84c2433e57805707645
Angeli <angeli.mittal@gmail.com>
2023-11-09 20:54:34 -0600

    idk

--- data_structures/Instruction.py
+++ data_structures/Instruction.py
@@ -4,6 +4,7 @@ from data_structures.ontologies.Ingredients import IngredientOntology
 from data_structures.ontologies.CookingTools import CookingToolsOntology
 from data_structures.Node import Node
 from nltk.stem.snowball import SnowballStemmer
+from fuzzywuzzy import fuzz
 import re
 
 stemmer = SnowballStemmer("english")
@@ -34,21 +35,25 @@ class Instruction(Node):
         return stemmer.stem(re.search(r'[A-Za-z]*', word).group(0))
 
     def __annotate(self, instruction_sent, ontology, variable):
-        for word in instruction_sent.split(" "):
+        for word in instruction_sent.lower().split(" "):
             word_stem = self.__get_word_stem(word)
             if ontology.get_category(word_stem) != "Unknown":
                 variable.append(word_stem)
+            elif ontology.get_category(word) != "Unknown": # word_stem might be weird
+                variable.append(word)
+            elif ontology.get_category(word_stem + 'e') != "Unknown": # word_stem might be weird
+                variable.append(word_stem + 'e')
 
-        variable = list(set(variable))
+        return list(set(variable))
 
     def __set_cooking_actions(self, instruction_sent):
-        self.__annotate(instruction_sent, cooking_actions, self.__cooking_actions)
+        self.__cooking_actions = self.__annotate(instruction_sent, cooking_actions, self.__cooking_actions)
 
     def __set_ingredients(self, instruction_sent):
-        self.__annotate(instruction_sent, ingredients, self.__ingredients)
+        self.__ingredients = self.__annotate(instruction_sent, ingredients, self.__ingredients)
 
     def __set_tools(self, instruction_sent):
-        self.__annotate(instruction_sent, tools, self.__tools)
+        self.__tools = self.__annotate(instruction_sent, tools, self.__tools)
 
     def get_cooking_actions(self):
         return self.__cooking_actions
--- data_structures/ontologies/CookingTools.py
+++ data_structures/ontologies/CookingTools.py
@@ -1,8 +1,8 @@
 class CookingToolsOntology:
     def __init__(self):
         self.categories = {
-            "Cutting Tools": ["knife", "scissors", "mandoline", "peeler", "grater", "zester", "shears", "cleaver", "paring knife", "bread knife"],
-            "Cooking Vessels": ["pot", "pan", "skillet", "wok", "saucepan", "roasting pan", "casserole dish", "stockpot", "Dutch oven", "grill pan"],
+            "Cutting Tools": ["knife", "scissors", "mandoline", "peeler", "grater", "zester", "shears", "cleaver", "paring knife", "bread knife", 'ricer'],
+            "Cooking Vessels": ["bowl", "pot", "pan", "skillet", "wok", "saucepan", "roasting pan", "casserole dish", "stockpot", "Dutch oven", "grill pan"],
             "Baking Tools": ["oven", "baking sheet", "rolling pin", "muffin tin", "cake pan", "cookie cutter", "pastry brush", "baking stone", "pie dish", "bread pan"],
             "Mixing Tools": ["spoon", "whisk", "spatula", "mixing bowl", "stand mixer", "hand mixer", "food processor", "blender", "measuring cups", "mixing spoon"],
             "Grilling Tools": ["grill", "tongs", "grill brush", "basting brush", "smoker box", "grill basket", "skewers", "heat-resistant gloves", "instant-read thermometer", "grill grates"],
